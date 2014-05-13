21a050a4f1e771b5783dbaf82623d800c50fdb1a
unknown <doylejd88@gmail.com>
2014-05-13 00:16:21 -0400

    Had some extra time and decided to add in match case to the search functionality. Also cleaned up the event listeners to be added through JS.

--- js/text_search.js
+++ js/text_search.js
@@ -2,6 +2,28 @@
  * Created by Doyle on 5/12/14.
  */
 
+/**
+ * Attach event listeners to the search form. Take into account older browser versions as well.
+ */
+if(window.addEventListener){
+    window.addEventListener("load", searchInit, false);
+} else if(window.attachEvent){ //IE < 9
+    window.attachEvent("onload", searchInit);
+} else{
+    document.addEventListener("load", searchInit, false);
+}
+
+/**
+ * Initialize the search button to execute our Javascript search.
+ */
+function searchInit(){
+    var button = document.getElementById("search-button");
+    if(button.addEventListener){
+        button.addEventListener("click", findOccurrences, false);
+    } else if(button.attachEvent){ //IE < 9
+        button.attachEvent("onclick", findOccurrences);
+    }
+}
 
 /**
  * function findOccurances() - Finds instances of a string within the given search text.
@@ -10,21 +32,29 @@
  * @param searchTerm
  * @returns {boolean}
  */
-function findOccurrences(searchTerm){
+function findOccurrences(){
     var position = 0;
     var occurrences = 0;
+
+    var matchCase = document.getElementById("match-case").checked;
+    var searchTerm = document.getElementById("search_input").value;
     var termLength = searchTerm.length;
-    var lowerTerm = searchTerm.toLowerCase();
+
+    var term = searchTerm;
     //Get the searchable content and strip out the HTML markup.
     var html = document.getElementById("search_text");
     var searchableContent = html.textContent || html.innerText || "";
 
+
     //Lowercase all of our content so that we can find all matches of the string.
-    searchableContent = searchableContent.toLowerCase();
+    if(matchCase == 0){
+        searchableContent = searchableContent.toLowerCase();
+        term = searchTerm.toLowerCase();
+    }
 
 
     while(true){
-        position = searchableContent.indexOf(lowerTerm, position);
+        position = searchableContent.indexOf(term, position);
         if(position >= 0){
             occurrences++;
             position += termLength;
